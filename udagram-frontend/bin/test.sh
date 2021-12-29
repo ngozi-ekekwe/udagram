978f7ddbe0c1717d77ced23bd9025fd6a954a719
Ngozi Ekekwe <ngozi-ekekwe>
2021-12-29 20:22:12 +0100

    test

--- udagram-frontend/bin/test.sh
+++ udagram-frontend/bin/test.sh
@@ -1,4 +1,4 @@
 env | grep CHROME_BIN
 export NODE_OPTIONS=--openssl-legacy-provider
 CHROME_BIN=/usr/bin/google-chrome 
-ng test --browsers=ChromeHeadless #--browsers=Headless --no-watch
+ng test --browsers=ChromeHeadless --watch=false #--browsers=Headless --no-watch
